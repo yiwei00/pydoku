c0536c298ef23bf1e9501eb0c889d374fd0beafc
Yi Wei <yiw00@outlook.com>
2018-11-13 01:45:48 -0500

    Clean up code and add comments.

--- pydoku.py
+++ pydoku.py
@@ -1,61 +1,9 @@
 import numpy as np
 
-#creates empty sudoku board
-board = np.zeros((9,9), dtype=np.int32) #for 2d arrays [row][col]
-read_board_from_file("sodoku.in")
-
-#function checks if the given row is valid
-def _is_row_valid(row):
-    assert row < 9 and row >= 0 , "row is out of range"
-    count = np.zeros((9,), dtype = np.int32)
-    for i in range(0,9):
-        if board[row][i] == 0:
-            continue
-        elif (count[board[row][i] - 1]) >= 1:
-            return False
-        else:
-            count[board[row][i] - 1] += 1
-    return True
-
-#function checks if the given column is valid
-def _is_col_valid(col):
-    assert col < 9 and col >= 0 , "column is out of range"
-    count = np.zeros((9,), dtype = np.int32)
-    for i in range(0,9):
-        if board[i][col] == 0:
-            continue
-        elif (count[board[i][col] - 1]) >= 1:
-            return False
-        else: 
-            count[board[i][col] - 1] += 1
-    return True
-
-#function checks if the given square is valid
-def _is_square_valid(sqaure_row, sqaure_col):
-    assert (sqaure_row < 3 or sqaure_col < 3) , "invalid square coordinates"
-    row = sqaure_row * 3
-    col = sqaure_col * 3
-    count = np.zeros((9,), dtype = np.int32)
-    for i in range(0,3):
-        for j in range(0,3):
-            if board[row + i][col + j] == 0:
-                continue
-            elif (count[board[row + i][col + j] - 1]) >= 1:
-                return False
-            else :
-                count[board[row + i][col + j] - 1] += 1
-    return True
-
-#function checks if the entire board is valid
-def _is_board_valid():
-    for i in range(0,9):
-        if (not _is_col_valid(i)) or (not _is_row_valid(i)):
-            return False
-    for i in range(0,3):
-        for j in range(0,3):
-            if (not _is_square_valid(i, j)):
-                return False
-    return True
+#read_board_from_file and write_board_to_file are functions for inputing and outputing
+#sodoku puzzles from files.
+#read_board_from_file reads a board from a file into memory, and write_board_to_file writes
+#a sodoku puzzle to a file from memory
 
 #reads in a board from a file
 #file should be in the format of 9 lines with 9 digits per line
@@ -82,3 +30,73 @@ def write_board_to_file(filename):
         line += "\n"
         outfile.write(line)
         line = ""
+
+#_is_row_valid, _is_col_valid, and _is_square_valid checks if
+#each aspect of the sodoku board (rows, column, and the 3x3 grid)
+#do not have duplicate values in their squares, and _is_board_valid
+#checks the entire board for validity
+
+#function checks if the given row is valid
+#takes in a row value 0-8, to check for validity
+#returns true if all values in the row is unique and false otherwise
+def _is_row_valid(row):
+    assert row < 9 and row >= 0 , "row is out of range"
+    count = np.zeros((9,), dtype = np.int32)
+    for i in range(0,9):
+        if board[row][i] == 0:
+            continue
+        elif (count[board[row][i] - 1]) >= 1:
+            return False
+        else:
+            count[board[row][i] - 1] += 1
+    return True
+
+#function checks if the given column is valid
+#takes in a column value 0-8 to check for validity
+#returns true if column is valid, and false otherwise
+def _is_col_valid(col):
+    assert col < 9 and col >= 0 , "column is out of range"
+    count = np.zeros((9,), dtype = np.int32)
+    for i in range(0,9):
+        if board[i][col] == 0:
+            continue
+        elif (count[board[i][col] - 1]) >= 1:
+            return False
+        else: 
+            count[board[i][col] - 1] += 1
+    return True
+
+#function checks if the given square is valid
+#takes in a row and column, and checks the validity of the 3x3 square
+#that coordinat falls in
+#returns true if the 3x3 square is valid, false otherwise
+def _is_square_valid(row, col):
+    assert ((row < 9 and row >= 0) or (col < 9 and col >= 0)) , "invalid square coordinates"
+    row = (row // 3) * 3
+    col = (col // 3) * 3
+    count = np.zeros((9,), dtype = np.int32)
+    for i in range(0,3):
+        for j in range(0,3):
+            if board[row + i][col + j] == 0:
+                continue
+            elif (count[board[row + i][col + j] - 1]) >= 1:
+                return False
+            else :
+                count[board[row + i][col + j] - 1] += 1
+    return True
+
+#function checks if the entire board is valid
+def _is_board_valid():
+    for i in range(0,9):
+        if (not _is_col_valid(i)) or (not _is_row_valid(i)):
+            return False
+    for i in range(0,3):
+        for j in range(0,3):
+            if (not _is_square_valid(i * 3 , j * 3)):
+                return False
+    return True
+
+
+#creates empty sudoku board
+board = np.zeros((9,9), dtype=np.int32) #for 2d arrays [row][col]
+read_board_from_file("sodoku.in")
